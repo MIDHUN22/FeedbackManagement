b5bc1c6ba0fd83f5b3e5dc39b1b5fcbe0fb2f899
MIDHUN M <midhunmadhusree@gmail.com>
2026-03-11 22:34:38 +0530

    regstration

--- Modules/Authentication.py
+++ Modules/Authentication.py
@@ -1,20 +1,19 @@
 import re
-from Modules.db import Database
+from Models.db import db
 class Authentication:
     # pass
-    def validate(self, name, password):
+    def validate(self, username, password):
 
         # name should contain only letters and spaces
-        if not re.fullmatch(r"[A-Za-z ]+", name):
-            print("Invalid name")
+        if not re.fullmatch(r"[A-Za-z ]+", username):
+            print("Invalid username")
             return False
 
         # password should not be empty
         if not password:
             print("Password cannot be empty")
             return False
-
-    return True
+        return True
 
     def register(self):
         print('____Register Your Self_____')
@@ -24,11 +23,32 @@ class Authentication:
         
         if not self.validate(username, password):
             return
-        print("Registration Successful!!!")
+
+        db.cursor.execute(
+            "SELECT 1 FROM users WHERE username=?",
+            (username,)
+        )
+        
+        if db.cursor.fetchone():
+            print("User already exists")
+            return
+
+        db.cursor.execute(
+            '''
+            INSERT INTO users(username,password,role)
+            VALUES(?,?,?)
+            ''',
+            (username, password, role)
+        )
+        if db.conn.commit():
+            print("Registration Successful!!!")
+        else:
+            print('Somethong Went Wrong')
+            return
 
     def Login(self):
         print("_______Login To Account_____")
-        name=input("Enter Your Name:\t").strip()
+        username=input("Enter Your Name:\t").strip()
         password=input("Enter Your Password:\t").strip()
         if not self.validate(username, password):
             return
